12939eebdf848adc6b81dce4d33123c133dcd3e8
maf1kkk <maksimbasuev@gmail.com>
2026-07-21 23:56:18 +0300

    Fix remove_prank.bat: delete specific Run values instead of entire key

--- remove_prank.bat
+++ remove_prank.bat
@@ -7,7 +7,8 @@ taskkill /f /im Prank.exe >nul 2>&1
 taskkill /f /im Prank_Test.exe >nul 2>&1
 taskkill /f /im Setup.exe >nul 2>&1
 
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /f >nul 2>&1
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v AudioService /f >nul 2>&1
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v WindowsCppRedist /f >nul 2>&1
 
 attrib -h -s "%ProgramData%\WindowsCppRedist" /s /d >nul 2>&1
 rd /s /q "%ProgramData%\WindowsCppRedist" >nul 2>&1
